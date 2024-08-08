371a03a2b99fed8fde02b9c04b0ee8efe6466d5c
Enzo Pelegrini <113308665+epelegrini@users.noreply.github.com>
2024-08-08 20:11:44 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
@@ -44,6 +45,7 @@ class User(db.Model):
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    role = SelectField('Role', choices=[('user', 'User'), ('mod', 'Moderator'), ('admin', 'Administrator')])
     submit = SubmitField('Submit')
 
 
